d9e614004e6f011c10c494c3032cae1fcd439568
B66780 <andrewrodriguez@fullsail.edu>
2014-05-01 23:30:57 -0400

    Update Rodriguez_Andrew_assignment4.js

--- Rodriguez_Andrew_assignment4.js
+++ Rodriguez_Andrew_assignment4.js
@@ -1,1 +1,27 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Andrew Rodriguez
+// Sdi - 1404	
+// Assignment 4 project 4
+// JavaScript Code Problems and Answers
+// "String", "Number", "Array"
+
+// String questions
+
+// 1. Checking A Phone Number
+// Does a string follow a 123-456-7890 pattern like a phone number?
+var PhoneNumCheck = function(val) {
+	var PhoneNumber = val;
+	var dash1 = (PhoneNumber.substring(3, 4));
+	var dash2 = (PhoneNumber.substring(7, 8));
+
+    if (dash1 == "_" && dash2 == "_") {
+        if (PhoneNumber.length == 12) {
+        return true;
+        } else {
+        	return false;
+
+        }
+    } // Conditional End
+
+}; // PhoneNumCheck Complete 
